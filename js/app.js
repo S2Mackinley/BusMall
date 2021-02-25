1b6b2980af0470cf3129ee3352614310ee816ff3
Elijah Prom <elijahprom@gmail.com>
2021-02-24 18:20:05 -0800

    added storage to site

--- js/app.js
+++ js/app.js
@@ -35,13 +35,39 @@ function Product(title, url) {
 Product.all = [];
 
 /* Functions */
-function createProducts() {
-  for (let index = 0; index < productNames.length; index++) {
-    const productName = productNames[index];
+function createProductsFromScratch() {
+  for (let i = 0; i < productNames.length; i++) {
+    const productName = productNames[i];
     new Product(productName, './imgs/' + productName + '.jpg');
   }
 }
 
+function createProductsFromStorage(storageGet) {
+  const javaScriptPics = JSON.parse(storageGet);
+  
+  for (let i = 0; i < javaScriptPics.length; i++) {
+    const rawData = javaScriptPics[i];
+    const newPictureInstance = new Product (rawData.title,rawData.url);
+    newPictureInstance.tally = rawData.tally;
+    newPictureInstance.views = rawData.views;
+    
+    
+  }
+}
+
+function createPicInstances() {
+  const storageGet = localStorage.getItem('totalClicks');
+  if(storageGet === null) {
+    createProductsFromScratch();
+  } else {
+    createProductsFromStorage(storageGet);
+  }
+
+}
+
+
+
+// picks new images while not repeating
 function renderNewProducts() {
   shuffle(Product.all);
   const noRepeat = [];
@@ -57,11 +83,11 @@ function renderNewProducts() {
 
 
   productLeft = noRepeat[0];
-  console.log(productLeft.title);
+  // console.log(productLeft.title);
   productCenter = noRepeat[1];
-  console.log(productCenter.title);
+  // console.log(productCenter.title);
   productRight = noRepeat[2];
-  console.log(productRight.title);
+  // console.log(productRight.title);
 
 }
 
@@ -127,10 +153,17 @@ function productClickHandler(event) {
 
   if (roundCtr === maxClicks) {
     imagesContainerElem.removeEventListener('click', productClickHandler);
+    const JSONImages = JSON.stringify(Product.all);
+    console.log(JSONImages);
+
+    localStorage.setItem('totalClicks', JSONImages);
+
+    // updateChartVotes();
+
     alert('You are out of clicks, Click "View Results" to see what you picked below');
     const resultsButton = document.getElementById('show-results');
     resultsButton.addEventListener('click',renderLikes);
-    
+
   }
 
 }
@@ -140,11 +173,13 @@ function productClickHandler(event) {
 function renderLikes() {
   const likesListELem = document.getElementById('results-list');
   likesListELem.innerHTML = '';
+  console.log(Product.all);
   for (let i = 0; i < Product.all.length; i++) {
       const itemProduct = Product.all[i];
       const itemProductElem = document.createElement('li');
-      likesListELem.appendChild(itemProductElem);
       itemProductElem.textContent = itemProduct.title + ' : ' + itemProduct.tally + ' clicks out of ' + itemProduct.views + " views.";
+      likesListELem.appendChild(itemProductElem);
+      
 
   }
   renderChart();
@@ -155,12 +190,18 @@ function renderLikes() {
 
 
 function renderChart() {
-
+let viewsArray = []
 let tallyArray = []
+let productTitle = []
 for (let index = 0; index < Product.all.length; index++) {
   const productTally = Product.all[index].tally;
+  const productViews =Product.all[index].views;
+  const productName = Product.all[index].title;
   tallyArray.push(productTally);
+  viewsArray.push(productViews);
+  productTitle.push(productName);
   
+
   
 }
 
@@ -172,15 +213,19 @@ for (let index = 0; index < Product.all.length; index++) {
 
     // The data for our dataset
     data: {
-      labels: productNames,
+      labels: productTitle,
       datasets: [{
-        label: 'Which Items Were Clicked Most',
+        label: 'How Many Times Product Was Clicked',
+        backgroundColor: 'rgb(69, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: tallyArray,
+      },
+      {
+        label: 'How many Times Products Were Viewed',
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
-
-        // TODO: get the "good" product data in here
-        data: tallyArray
-      }]
+        data: viewsArray
+    }] 
     },
 
     // Configuration options go here
@@ -189,20 +234,42 @@ for (let index = 0; index < Product.all.length; index++) {
 }
 
 
-
 imagesContainerElem.addEventListener('click', productClickHandler);
 
+// Before i create my products i need to check if i have products already in local storage. 
 
-createProducts();
+// if i do im going to use those products. 
+
+// else if i dont i need to call the create product function
+
+
+// if (localStorage.getItem('totalClicks') !== null) {
+//   console.log('You Have Saved Dated');
+//   Product.all = JSON.parse(localStorage.getItem('totalClicks'));
+// } else {
+//   console.log('You Have No Saved Data');
+//   localStorage.setItem('totalClicks', JSON.stringify(Product.all));
+// }
+// console.log('what?',localStorage);
+
+// function updateChartVotes() {
+//   for (var i = 0; i < Product.all.length; i++) {
+//     tallyArray = Product.all[i].tally;
+//     // console.log('tally',tallyArray)
+
+//     //what
+//   }
+// }
+
+createPicInstances();
+
 
 renderNewProducts();
 
 renderProducts();
 
-// renderLikes();
 
 
+// localStorage.clear();
 
 
-// call when done
-// renderChart();
